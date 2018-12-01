2550dd14e88a4a03d022e4d54611fd5f43859e7c
Florentin C <florentin.coyard@gmail.com>
2018-12-01 13:25:48 +0100

    git ignore

--- back-into-the-wild/index.js
+++ back-into-the-wild/index.js
@@ -4,3 +4,10 @@ const connection = require("./conf");
 const app = express();
 const port = 3010;
 const cors = require('cors');
+
+app.listen(port, err => {
+  if (err) {
+    throw new Error('Something went wrong ...');
+  }
+  console.log(`Server is listening on ${port}`)
+});
